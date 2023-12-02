1f6bafc1c342ffd32942bf801ddfaea4983c340a
kamal <kamak.@gmail.com>
2023-12-02 14:45:28 +0400

    projrct

---
+++ Break-continue/test.js
@@ -0,0 +1,17 @@
+// dondurmaq
+// for (let ay = 1; ay < 60; ay++) {
+//     for (let gun = 1; gun < 31; gun++) {
+//         for (let i = 0; i < 24; i++) {
+//             for (let a = 0; a <= 59; a++) {
+//                 for (let j = 0; j <= 59; j++) {
+//                     console.log(`${ ay } - ci ayın ${ gun } - ci gunu ${ i }: ${ a }: ${ j }`)
+//                 }
+
+//             }
+//         }
+//     }
+// }
+
+// console.log('salam dunya \n sagol dunya \n kevin andrey');
+
+
